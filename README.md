1fbc4be9be9c4491c7f435cc8c0a55f2a5318f36
sth0409 <97033765@qq.com>
2021-11-12 16:32:59 +0800

    更新demo1031

---
+++ README.md
@@ -0,0 +1,115 @@
+# 乐福乐alldls_Android接入文档1.0.3.1
+
+## 1. SDK接入
+
+#### 1.1 申请AppId
+
+请向相关人员申请 appId
+
+## 2. 引用SDK
+
+```
+maven {
+  credentials {
+      username 'rvxtfz'
+      password 'QBWaa4Fi8O'
+      	}
+      url 'https://packages.aliyun.com/maven/repository/2046311-release-HZhbV0/'
+   	}
+maven { url 'https://repo1.maven.org/maven2/' }
+
+implementation 'com.alldls.lflsdk:lflsdk:1.0.3.1'
+///!!!重要说明！！！需要额外引用最新版融合SDK
+```
+
+### 3.乐福乐SDK初始化
+
+请在Application中调用初始化方法
+
+```java
+LflSDK.init(Application application, String appId);
+```
+
+## 4.乐福乐SDK有两种方式,选择其中一种即可
+
+#### 4.1 直接跳转到SDK内部乐福乐Activity
+
+```
+LflSDK.show(Context context, String appId, String userId, EventListener eventListener);
+```
+
+#### 4.2 引用LflTasksLayout
+
+```
+LflLayout lflTasksLayout= (LflLayout) findViewById(R.id.LflTasksLayout);
+
+lflTasksLayout.onLoadLayout(Activity activity, String appId, String userId,EventListener eventListener);
+
+注意！此接入方式一定要在onBackPressed中调用
+@Override
+public void onBackPressed() {
+if (lflTasksLayout.getWebView().canGoBack()) {
+   lflTasksLayout.getWebView().goBack();
+} else {
+   super.onBackPressed();
+  }
+}
+注意！此接入方式一定要在onDestroy中调用
+  lflLayout.onDestroy();
+```
+
+EventListener类：
+
+```java
+public interface EventListener {
+    /***
+     * 乐福乐页面关闭
+     */
+    void onPageClose();
+
+}
+```
+
+## 5 自定义任务回调说明
+
+#### 5.1 在适当位置添加自定义任务回调
+
+```java
+LflSDK.addListener(LflCustomTaskListener listener)
+```
+
+#### 5.2 当用户操作了自定义任务需要调用如下代码通知乐福乐SDK
+
+```java
+//任务完成
+LflSDK.triggerSuccess(CustomTaskType customTask);
+
+//任务失败
+LflSDK.triggerFail(CustomTaskType customTask);
+```
+
+CustomTaskType类
+
+```java
+public enum CustomTaskType {
+
+    TAKE_PHOTO("拍照", 5),
+
+    SHARE("分享", 6),
+
+    INVITE("邀请", 7);
+}
+```
+
+LflCustomTaskListener类：
+
+```java
+public interface LflCustomTaskListener {
+    /**
+     * 调用自定义任务(当用户点击页面的自定义任务时会响应此回调)
+     *
+     * @param customTask
+     */
+    void onCallCustomTask(CustomTaskType customTsk);
+}
+```
--- app/src/main/java/com/lflsdkdemo/android/MainActivity.java
+++ app/src/main/java/com/lflsdkdemo/android/MainActivity.java
@@ -1,14 +1,79 @@
 package com.lflsdkdemo.android;
 
+import android.app.Application;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+//import com.happytour.lflsdk.CustomTaskType;
+//import com.happytour.lflsdk.LflSDK;
+//import com.happytour.lflsdk.api.EventListener;
+//import com.happytour.lflsdk.api.LflCustomTaskListener;
+//
+import com.alldls.lflsdk.CustomTaskType;
+import com.alldls.lflsdk.LflSDK;
+import com.alldls.lflsdk.api.EventListener;
+import com.alldls.lflsdk.api.LflCustomTaskListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etAppId;
+    private EditText etUserId;
+    private Button btnGo;
+
+    private String appId = "";
+    private String userId = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        etAppId = (EditText) findViewById(R.id.et_app_id);
+        etUserId = (EditText) findViewById(R.id.et_user_id);
+        btnGo = (Button) findViewById(R.id.btn_go);
+
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(etAppId.getText().toString().trim())) {
+                    Toast.makeText(MainActivity.this, "please set app id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                appId = etAppId.getText().toString().trim();
+                if (TextUtils.isEmpty(etUserId.getText().toString().trim())) {
+                    Toast.makeText(MainActivity.this, "please set user id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                userId = etUserId.getText().toString().trim();
+                //初始化sdk
+                LflSDK.init((Application) MainActivity.this.getApplicationContext(), appId);
+                //添加自定义任务回调（可选）
+                LflSDK.addListener(new LflCustomTaskListener() {
+                    @Override
+                    public void onCallCustomTask(CustomTaskType customTaskType) {
+                        Toast.makeText(MainActivity.this, customTaskType.typeName, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                //打开乐福乐页面
+                LflSDK.show(MainActivity.this, appId, userId, new EventListener() {
+                    @Override
+                    public void onPageClose() {
+                        Toast.makeText(MainActivity.this, "onPageClose", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
